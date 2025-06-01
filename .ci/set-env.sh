f69023a7d133de2f4ea78e2a359d3a6f621e25ce
0x1def <0x1def@flakybit.net>
2025-06-01 22:07:35 +0300

    Version & deps bump

--- .ci/set-env.sh
+++ .ci/set-env.sh
@@ -14,7 +14,7 @@ app_component='server'
 printf 'APP_COMPONENT=%s\n' "$app_component" >> "$CI_ENV_FILE"
 
 printf 'APP_VERSION=%s\n' "$(getAppVersion)" >> "$CI_ENV_FILE"
-printf 'BLOCKY_VERSION=%s\n' 'v0.25' >> "$CI_ENV_FILE"
+printf 'BLOCKY_VERSION=%s\n' 'v0.26.2' >> "$CI_ENV_FILE"
 
 printf 'HARBOR_REGISTRY=%s\n' 'harbor.flakybit.net' >> "$CI_ENV_FILE"
 printf 'EXTERNAL_REGISTRY_NAMESPACE=%s\n' 'flakybitnet' >> "$CI_ENV_FILE"
